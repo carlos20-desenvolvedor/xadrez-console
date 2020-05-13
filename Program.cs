eefd8b7aace18ce1927aa2e8e36f4dd7872f64e4
carlossantos.desenvolvedor@gmail.com <carlossantos.desenvolvedor@gmail.com>
2020-05-12 23:25:02 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            {
+                Console.ReadLine();
+            }
+        }
+        
+    }
+}
